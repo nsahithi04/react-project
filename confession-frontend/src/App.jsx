f0c02a721ac6fd40457ede9f34e3f8c27b6679ea
nsahithi04 <sahithi.nampally@gmail.com>
2026-03-15 22:25:49 -0700

    configured routing

--- confession-frontend/src/App.jsx
+++ confession-frontend/src/App.jsx
@@ -1,21 +1,25 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Form from "./form";
 import Otp from "./verifyOtp";
 import Home from "./home";
 import { ViewConfessions, AddConfession } from "./home";
 
+const router = createBrowserRouter([
+  { path: "/", element: <Form /> },
+  { path: "/otp", element: <Otp /> },
+  {
+    path: "/home",
+    element: <Home />,
+    children: [
+      { index: true, element: <ViewConfessions /> },
+      { path: "add", element: <AddConfession /> },
+    ],
+  },
+]);
+
 function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<Form />} />
-      <Route path="/otp" element={<Otp />} />
-      <Route path="/home" element={<Home />}>
-        <Route index element={<ViewConfessions />} />
-        <Route path="add" element={<AddConfession />} />
-      </Route>
-    </Routes>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
--- confession-frontend/src/index.jsx
+++ confession-frontend/src/index.jsx
@@ -5,8 +5,6 @@ import App from "./App";
 
 import { Provider } from "react-redux";
 
-import { BrowserRouter as Router } from "react-router-dom";
-
 import { PersistGate } from "redux-persist/integration/react";
 import { store, persistor } from "./store/store";
 
@@ -15,9 +13,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
-      <Router>
-        <App />
-      </Router>
+      <App />
     </PersistGate>
   </Provider>,
 );
